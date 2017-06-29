cf06d0120d55bddf0292d2b95c305b1c01e34158
glassjoseph <glassjoseph@gmail.com>
2017-06-29 16:03:30 -0600

    add seeds for meals and food

--- db/migrations/20170629153939_create-foods-table.js
+++ db/migrations/20170629153939_create-foods-table.js
@@ -3,6 +3,7 @@ exports.up = function(knex, Promise) {
   let createQuery = `CREATE TABLE foods(
     id SERIAL PRIMARY KEY NOT NULL,
     name TEXT,
+    calories INTEGER,
     created_at TIMESTAMP
   )`;
   return knex.raw(createQuery)
