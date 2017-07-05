eefa229434560e97094eb473bf6063ecf40f1b91
Valerie Trudell <v.trudell@protonmail.com>
2017-07-05 15:19:44 -0600

    Adds foods endpoint test

--- server.js
+++ server.js
@@ -23,51 +23,21 @@ app.get('/', (request, response) => {
   response.send("Welcome to our app!")
 })
 
+//NEW
 
-// app.get('/api/v1/foods', (request, response) => {
-//   response.json({name: "Prociottio", calories: 60})
-// })
+app.get('/api/v1/foods', FoodsController.index)
+app.get('/api/v1/foods/:id', FoodsController.show)
+app.post('/api/v1/foods', FoodsController.create)
+app.put('/api/v1/foods/:id', FoodsController.update)
+app.delete('/api/v1/foods/:id', FoodsController.delete)
 
-// app.get('/api/v1/meals', (request, response) => {
-//   response.json({name: "lunch"})
-// })
+app.get('/api/v1/meals', MealsController.index)
+app.get('/api/v1/meals/:id', MealsController.show)
 
 if(!module.parent){
   app.listen(app.get('port'), () => {
     console.log(`${app.locals.title} is running on ${app.get('port')}`)
   })
 }
-
-//NEW
-
-app.get('/api/v1/foods', FoodsController.index)
-app.get('/api/v1/foods/:id', FoodsController.show)
-app.post('/api/v1/foods', FoodsController.create)
-
-// app.post('/api/v1/foods', (request, response) => {
-//   FoodsController.create(request, response)
-// })
-
-app.put('/api/v1/foods/:id', (request, response) => {
-  FoodsController.update(request, response)
-})
-
-
-
-app.delete('/api/v1/foods/:id', (request, response) => {
-
-  FoodsController.delete(request, response)
-})
-
-
-
-app.get('/api/v1/meals', (request, response) => {
-  MealsController.index(request, response)
-})
-
-app.get('/api/v1/meals/:id', (request, response) => {
-  // eval(pry.it)
-  MealsController.show(request, response)
-})
 
 module.exports = app
--- test/controllers/foodsControllerTest.js
+++ test/controllers/foodsControllerTest.js
@@ -2,59 +2,105 @@ const pry            = require('pryjs')
 const assert         = require("chai").assert
 const app            = require('../../server')
 const request        = require('request')
-const Food           = require('../../lib/models/food')
-
-const environment    = process.env.NODE_ENV || 'test'
+const environment    = process.env.NODE_ENV || 'development'
 const configuration  = require('../../knexfile')[environment]
 const database       = require('knex')(configuration)
 
 describe("Food Endpoint", function() {
-  this.timeout(9001)
+  this.timeout(100000)
+  before(function(done) {
+    this.port = 9001
 
-  before((done) => {
-    this.port   = 9001
-    this.server = app.listen(this.port, (error, result) => {
-      if (error) { return done(err) }
+    this.server = app.listen(this.port, function(error, result) {
+      if(error) {return done(err) }
       done()
     })
 
-    this.request = request.defaults({
-      baseUrl: 'http://localhost:9001'
-    })
+    this.request = request.defaults(
+      {baseUrl: 'http://localhost:9001'}
+    )
   })
 
-  after(() => {
+  after(function(done) {
     this.server.close()
+    done()
   })
 
-  it("GET api/v1/foods", function(done) {
-    this.request.get("api/v1/foods", function(error, resposne) {
+  it("GET /api/v1/foods", function(done) {
+    this.request.get('/api/v1/foods', function(error, response) {
       if(error) {done(error)}
       const parsed = JSON.parse(response.body)
+
       assert.equal(response.statusCode, 200)
-      //assert.equal(parsed.length, 5)
+      assert.equal(parsed.length, 4)
       done()
     })
   })
 
-  xit("GET /api/v1/foods/:id", function() {
-    this.request.get("api/v1/foods/1", function(error, response) => {
+  it("GET /api/v1/foods/:id", function(done) {
+    this.request.get('/api/v1/foods/1', function(error, response) {
       if(error) {done(error)}
       const parsed = JSON.parse(response.body)
+
       assert.equal(response.statusCode, 200)
       assert.equal(parsed.length, 1)
+      done()
     })
   })
 
-  it("POST /api/v1/foods", function() {
-
+  it("GET an id that doesn't exist", function(done) {
+    this.request.get('/api/v1/foods/9001', function(error, response) {
+      assert.equal(response.statusCode, 404)
+      done()
+    })
   })
 
-  it("PUT /api/v1/foods/:id", function() {
+  it("POST /api/v1/foods", function(done) {
+    this.request.post('/api/v1/foods?name=Chocolate&calories=100', function(error, response) {
+      if(error) {done(error)}
+      const parsed = JSON.parse(response.body)
 
+      assert.equal(response.statusCode, 200)
+      assert.equal(parsed[0].name, "Chocolate")
+      assert.equal(parsed[0].calories, 100)
+      assert.equal(parsed[0].id, 5)
+      done()
+    })
   })
 
-  it("DELETE /api/v1/foods/:id", function() {
+  it("PUT /api/v1/foods/:id", function(done) {
+    this.request.put('/api/v1/foods/5?name=Tuna Sandwhich&calories=80', function(error, response) {
+      if(error) {done(error)}
+      const parsed = JSON.parse(response.body)
 
+      assert.equal(response.statusCode, 200)
+      assert.equal(parsed[0].name, "Tuna Sandwhich")
+      assert.equal(parsed[0].calories, 80)
+      assert.equal(parsed[0].id, 5)
+      done()
+    })
+  })
+
+  it("PUT for an id that doens't exist", function(done) {
+    this.request.put('/api/v1/foods/9001?name=pleeb&calories=330', function(error, response) {
+      assert.equal(response.statusCode, 404)
+      done()
+    })
+  })
+
+  it("DELETE /api/v1/foods/:id", function(done) {
+    this.request.delete('/api/v1/foods/5', function(error, response) {
+      if(error) {done(error)}
+      assert.equal(response.statusCode, 204)
+      assert.equal(response.body.length, 0)
+    done()
+    })
+  })
+
+  it("DELETE an id that doesn't exist", function(done) {
+    this.request.delete('/api/v1/foods/9001', function(error, response) {
+      assert.equal(response.statusCode, 404)
+    done()
+    })
   })
 })
