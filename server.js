a60ff3041eb9f64c7d5ef071ad5c6103eeff3b0e
glassjoseph <glassjoseph@gmail.com>
2017-07-12 18:03:56 -0600

    Modify food update path to take name
    - server.js - change path to /:name
    - split update() into update_name & update_calories

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/', (request, response) => {
 app.get('/api/v1/foods', FoodsController.index)
 app.get('/api/v1/foods/:name', FoodsController.show)
 app.post('/api/v1/foods', FoodsController.create)
-app.put('/api/v1/foods/:id', FoodsController.update)
+app.put('/api/v1/foods/:name', FoodsController.update)
 app.delete('/api/v1/foods/:name', FoodsController.delete)
 
 app.get('/api/v1/meals', MealsController.index)
